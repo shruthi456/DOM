593345ae290b1df634f6bed6b478e8795e83762e
shruthi reddy <shruthireddy583@gmail.com>
2024-02-07 15:13:13 +0530

    .

---
+++ data4.js
@@ -0,0 +1,8 @@
+
+function changecolor(){
+   // alert('shruthi')
+    let btn_ref = document.getElementById('mno')
+    console.log(btn_ref)
+    //btn_ref.innerHTML  = "GM"
+    btn_ref.style.backgroundColor = 'purple'
+}
